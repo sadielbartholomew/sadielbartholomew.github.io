8846a3f9a91fbb758773d99325d7397494558bf5
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2020-03-19 19:23:48 +0000

    Convert content to YAML for more flexibility & adapt rebuild script

--- rebuild-site.py
+++ rebuild-site.py
@@ -1,9 +1,10 @@
 import os
-from jinja2 import Environment, PackageLoader
-from markdown2 import markdown
+from jinja2 import Environment, FileSystemLoader
+import yaml
+# import pprint  # debugging
 
 # Define the HTML templates which are processed by Jinja2
-jinja2_env = Environment(loader=PackageLoader('rebuild-site', 'templates'))
+jinja2_env = Environment(loader=FileSystemLoader('./templates'))
 root_homepage_template = jinja2_env.get_template('index.html')
 creative_homepage_template = jinja2_env.get_template('subindex-creative.html')
 technical_homepage_template = jinja2_env.get_template(
@@ -11,26 +12,27 @@ technical_homepage_template = jinja2_env.get_template(
 
 # Define which template shoudl be applied which each content set
 content_to_template_mapping = {
-    'index.md': root_homepage_template,
+    'index.yml': root_homepage_template,
 }
-for page in os.listdir('content'):
+for page in os.listdir('content'):  # assume all YAML files (as standard)
     if page.startswith('creative-sub'):
         content_to_template_mapping[page] = creative_homepage_template
     elif page.startswith('technical-sub'):
         content_to_template_mapping[page] = technical_homepage_template
 
+# Populate templates with content and write to files as the rendered pages.
+# Process global content first:
+with open(os.path.join('content', 'global.yml')) as file:
+    global_content = yaml.load(file.read())
 for content_set, template in content_to_template_mapping.items():
-    # Process the markdown content keys and values
+    # Process the local (page-specific) content keys and values
     file_path = os.path.join('content', content_set)
     with open(file_path, 'r') as file:
-        page = markdown(file.read(), extras=['metadata'])
+        content = yaml.load(file.read(), Loader=yaml.BaseLoader)
+    # pprint.pprint(content)  # debugging
 
     # Render each post to '<md file name>.html' under top-level repo dir
-    data = {'content': page}
-    data.update(page.metadata)
-    page_html = template.render(thispage=data)
-
-    print(data, page_html) ### debug
+    page_html = template.render(page=content, site_global=global_content)
     page_file_path = './{}.html'.format(content_set.rsplit(".", 1)[0])
     os.makedirs(os.path.dirname(page_file_path), exist_ok=True)
     with open(page_file_path, 'w') as file:
